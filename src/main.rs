322bd9096ff64cde7f108b233bbcbb279c9549eb
Ariel Ben-Yehuda <ariel.byd@gmail.com>
2017-10-30 22:14:42 +0200

    ready for rpass

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,10 @@ extern crate serde;
 extern crate toml;
 extern crate fmt_macros;
 
-use std::collections::HashMap;
-use std::env;
-use std::fs::File;
-use std::io::Read;
+use std::fs;
+use std::io::{self, Read};
 use std::process::Command;
+use std::path::PathBuf;
 
 pub const COMPILETEST_COMMAND: &[&str] = &[
     "{BUILDROOT}/{HOST}/stage0-tools/x86_64-unknown-linux-gnu/release/compiletest",
@@ -23,13 +22,13 @@ pub const COMPILETEST_COMMAND: &[&str] = &[
     "--rustc-path",
     "{BUILDROOT}/{HOST}/stage1/bin/rustc",
     "--src-base",
-    "{SRCROOT}/src/test/{MODE}",
+    "{SRCROOT}/src/test/{SUITE}",
     "--build-base",
-    "{CACHEDIR}/{MODE}/{KIND}",
+    "{TARGETDIR}",
     "--stage-id",
     "stage1-{HOST}",
     "--mode",
-    "{MODE}",
+    "{SUITE}",
     "--target",
     "{HOST}",
     "--host",
@@ -86,65 +85,130 @@ quick_main!(run);
 
 #[derive(Clone, Deserialize)]
 struct Configuration {
-    python: String,
-    rust_buildroot: String,
-    rust_srcroot: String,
+    python: PathBuf,
+    rust_buildroot: PathBuf,
+    rust_srcroot: PathBuf,
     host: String,
-    cachedir: String,
+    cachedir: PathBuf,
+    blessed_dir: PathBuf,
+}
+
+impl Configuration {
+    fn target_dir(&self, mode: &str, kind: &str) -> PathBuf {
+        self.cachedir.join(mode).join(kind)
+    }
+}
+
+fn cvpath(p: &PathBuf) -> Result<String> {
+    match p.to_str() {
+        Some(s) => Ok(s.to_owned()),
+        None => bail!("bad character in path `{:?}`",
+                      p.to_string_lossy()),
+    }
 }
 
 fn run_tester(cfg: &Configuration,
-              mode: &str,
+              suite: &str,
               kind: &str,
               extra_flags: &str) -> Result<()> {
-    info!("running tests in configuration {:?} {:?} {:?}", mode, kind, extra_flags);
+    info!("running tests in configuration {:?} {:?} {:?}", suite, kind, extra_flags);
     let args = COMPILETEST_COMMAND.iter().map(|c| {
         let parser = fmt_macros::Parser::new(c);
         parser.map(|p| {
             match p {
                 fmt_macros::Piece::String(s) => Ok(s.to_owned()),
-                fmt_macros::Piece::NextArgument(a) => Ok(match a.position {
+                fmt_macros::Piece::NextArgument(a) => match a.position {
                     fmt_macros::Position::ArgumentNamed("PYTHON") =>
-                        cfg.python.clone(),
+                        cvpath(&cfg.python),
                     fmt_macros::Position::ArgumentNamed("BUILDROOT") =>
-                        cfg.rust_buildroot.clone(),
+                        cvpath(&cfg.rust_buildroot),
                     fmt_macros::Position::ArgumentNamed("SRCROOT") =>
-                        cfg.rust_srcroot.clone(),
+                        cvpath(&cfg.rust_srcroot),
+                    fmt_macros::Position::ArgumentNamed("TARGETDIR") =>
+                        cvpath(&cfg.target_dir(suite, kind)),
                     fmt_macros::Position::ArgumentNamed("HOST") =>
-                        cfg.host.clone(),
+                        Ok(cfg.host.clone()),
+                    fmt_macros::Position::ArgumentNamed("SUITE") =>
+                        Ok(suite.to_owned()),
                     fmt_macros::Position::ArgumentNamed("CACHEDIR") =>
-                        cfg.cachedir.clone(),
-                    fmt_macros::Position::ArgumentNamed("MODE") =>
-                        mode.to_owned(),
-                    fmt_macros::Position::ArgumentNamed("KIND") =>
-                        kind.to_owned(),
+                        cvpath(&cfg.cachedir),
                     fmt_macros::Position::ArgumentNamed("EXTRA_FLAGS") =>
-                        extra_flags.to_owned(),
+                        Ok(extra_flags.to_owned()),
                     fmt_macros::Position::ArgumentNamed(a) =>
                         bail!("bad var {:?}", a),
                     _ => unreachable!()
-                })
+                }
             }
         }).collect()
     }).collect::<Result<Vec<String>>>()?;
 
     let status = Command::new(&args[0]).args(&args[1..]).status()?;
-    info!("running tests in configuration {:?} - status={:?}", mode, status);
+    info!("running tests in configuration {:?} {:?} - status={:?}",
+          suite, kind, status);
     Ok(())
 
 }
 
-fn collect_data(cfg: &Configuration, suite: &str) -> Result<()> {
+fn on_suite(cfg: &Configuration, suite: &str) -> Result<()> {
     run_tester(cfg, suite, "ast", "")?;
     run_tester(cfg, suite, "mir", "-Z borrowck-mir")?;
+
+    for file in fs::read_dir(cfg.target_dir(suite, "ast"))? {
+        let file = file?;
+        let filename = file.file_name();
+        let filename = match filename.into_string() {
+            Ok(s) => s,
+            Err(_) => continue
+        };
+        if !filename.ends_with(".err") {
+            continue
+        }
+        info!("comparing test {:?}", filename);
+
+        let mut ast_result = String::new();
+        fs::File::open(file.path())?.read_to_string(&mut ast_result)?;
+
+        let mut mir_result = String::new();
+        let mir_path = cfg.target_dir(suite, "mir").join(&filename);
+        let mut mir_file = match fs::File::open(mir_path) {
+            Ok(f) => f,
+            Err(ref e) if e.kind() == io::ErrorKind::NotFound => {
+                // file was not compiled due to other errors
+                println!("X {}/{}", suite, filename);
+                continue
+            }
+            Err(e) => return Err(From::from(e)),
+        };
+        mir_file.read_to_string(&mut mir_result)?;
+
+        if ast_result != mir_result {
+            let blessed_path = cfg.blessed_dir.join(&filename);
+            match fs::File::open(blessed_path) {
+                Ok(mut f) => {
+                    let mut blessed_result = String::new();
+                    f.read_to_string(&mut blessed_result)?;
+                    if mir_result != blessed_result {
+                        println!("R {}/{}", suite, filename);
+                        continue
+                    }
+                }
+                Err(ref e) if e.kind() == io::ErrorKind::NotFound => {
+                    // file is not blessed
+                    println!("E {}/{}", suite, filename);
+                    continue
+                }
+                Err(e) => return Err(From::from(e)),
+            }
+        }
+    }
     Ok(())
 }
 
 fn run() -> Result<i32> {
     env_logger::init().expect("logger initialization successful");
     let mut cfg = String::new();
-    File::open("nll-probe.toml")?.read_to_string(&mut cfg)?;
+    fs::File::open("nll-probe.toml")?.read_to_string(&mut cfg)?;
     let cfg: Configuration = toml::from_str(&cfg)?;
-    collect_data(&cfg, "run-pass")?;
+    on_suite(&cfg, "run-pass")?;
     Ok((0))
 }
